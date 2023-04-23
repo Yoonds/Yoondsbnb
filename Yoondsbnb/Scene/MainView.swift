dcf1e8543c12ccbb4342a4dcfeb45ec9a1e70300
YoonDaeSung <wkzks123@naver.com>
2023-04-24 08:31:05 +0900

    feature: 디자인 생성 #1 - 검색바 디자인 생성 - 하단 탭바 생성 - contentView 리네이밍

--- Yoondsbnb/Scene/MainView.swift
+++ Yoondsbnb/Scene/MainView.swift
@@ -7,16 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
+					SearchBar()
+						.padding(.horizontal, 20)
 					MainTabView()
         }
-        .padding()
     }
 }
 
+struct SearchBar: View {
+
+	@State var serachText: String = ""
+	
+	var body: some View {
+		HStack {
+			Image(systemName: "magnifyingglass")
+			VStack {
+				TextField("어디로 여행가세요?", text: $serachText)
+				Text("어디든지 언제든 일주일 게스트 추가")
+			}
+			Spacer()
+		}
+		.font(.headline)
+		.padding(10)
+		.background(
+			RoundedRectangle(cornerSize: CGSize(width: 30.0, height: 30.0))
+				.fill(Color.white)
+				.shadow(
+					color: .gray.opacity(0.3),
+					radius: 15, x: 0, y: 0
+				)
+		)
+	}
+	
+}
+
 struct MainTabView: View {
+	
 	var body: some View {
 		TabView {
 			Text("Search Tab")
@@ -26,30 +55,31 @@ struct MainTabView: View {
 				}
 			Text("WishList Tab")
 				.tabItem {
-					Image(systemName: "magnifyingglass")
+					Image(systemName: "heart")
 					Text("위시리스트")
 				}
 			Text("Travel Tab")
 				.tabItem {
-					Image(systemName: "magnifyingglass")
+					Image(systemName: "paperplane")
 					Text("여행")
 				}
 			Text("Message Tab")
 				.tabItem {
-					Image(systemName: "magnifyingglass")
+					Image(systemName: "message")
 					Text("메시지")
 				}
 			Text("Profile Tab")
 				.tabItem {
-					Image(systemName: "magnifyingglass")
+					Image(systemName: "person.crop.circle")
 					Text("프로필")
 				}
 		}
 	}
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
