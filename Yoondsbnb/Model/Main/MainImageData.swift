0c9bd470e917bc724832e4f77bd5f2ee3896462e
Yoonds <wkzks123@naver.com>
2023-10-20 21:44:20 +0900

    feature: SearchView 업데이트 - price 포매팅 생성 - view 업데이트 및 구조 세팅

--- Yoondsbnb/Model/Main/MainImageData.swift
+++ Yoondsbnb/Model/Main/MainImageData.swift
@@ -10,8 +10,9 @@ import Foundation
 struct MainImageResponse: Identifiable, Hashable {
 	
 	var id = UUID()
-	var image: String // 이미지 배열로 받은 후 그리드 노출 필요
-	var area: String
+	var image: String // FIXME: 이미지 배열로 받은 후 그리드 노출 필요
+    var country: String
+	var region: String
 	var description: String
 	var startDate: String // FIXME: Date 타입 변경 필요
 	var endDate: String
