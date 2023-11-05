42dd6dd6413132acfa27784bbdf20ffec007df49
Yoonds <wkzks123@naver.com>
2023-11-05 22:45:00 +0900

    design: 테스트 이미지 추가

--- Yoondsbnb/Scene/Search/SubViews/Listing/ListingDetailView.swift
+++ Yoondsbnb/Scene/Search/SubViews/Listing/ListingDetailView.swift
@@ -16,15 +16,16 @@ struct ListingDetailView: View {
 		"mainTest4"
 	]
 	
-    var body: some View {
+	var body: some View {
 		ScrollView {
 			ListingImageCarouselView()
-				.frame(width: 320)
+			//				.frame(width: 320)
 			
-			VStack(alignment: .leading, spacing: 8) {
+			VStack(alignment: .leading, spacing: 0) {
 				Text("한국 서울")
 					.font(.title)
 					.fontWeight(.semibold)
+					.padding(.bottom, 15)
 				
 				VStack(alignment: .leading) {
 					HStack(spacing: 2) {
@@ -49,43 +50,81 @@ struct ListingDetailView: View {
 					}
 				}
 				.font(.caption)
-			}
-			.padding(.leading)
-			.frame(maxWidth: .infinity, alignment: .leading)
-			
-			Divider()
-			
-			HStack(spacing: 20) {
-				Text("흔치 않은 기회입니다. 복선님의 에어비엔비 보통 예약이 가득 차 있습니다.")
-					.font(.footnote)
-					.frame(width: 300)
-				Image(systemName: "diamond")
-					.foregroundColor(.cyan)
-			}
-			
-			Divider()
-			
-			
-			
-			VStack(alignment: .leading, spacing: 0) {
+				.padding(.bottom, 15)
+				
+				HStack(spacing: 20) {
+					Text("흔치 않은 기회입니다. 복선님의 에어비엔비 보통 예약이 가득 차 있습니다.")
+						.font(.footnote)
+					
+					Spacer()
+					
+					Image(systemName: "diamond")
+						.foregroundColor(.cyan)
+				}
+				.padding(.bottom, 15)
+				
+				Divider()
+				
 				HStack(spacing: 0) {
 					Text("복선")
 					Text(" 님이")
 					Text(" 호스팅하는")
 					Text(" B&b")
 					Text("의 방")
-						.padding(.trailing, 20)
+					
+					Spacer()
+					
 					Image("mainTest")
 						.resizable()
+						.scaledToFill()
+						.frame(width: 64, height: 64)
+						.clipShape(Circle())
+						.overlay(alignment: .bottomTrailing) {
+							Image(systemName: "person.fill.turn.down")
+								.foregroundColor(.red)
+						}
 				}
+				.padding(.vertical, 15)
+				
+				Divider()
+					.padding(.bottom, 15)
+				
+				VStack(alignment: .leading, spacing: 15) {
+					HStack(alignment: .top, spacing: 0) {
+						Image(systemName: "person.fill.turn.down")
+						
+						VStack(spacing: 0) {
+							Text("b&b의 방")
+							Text("Aibiya님은 슈퍼호스트 입니다.")
+						}
+					}
+					HStack(alignment: .top, spacing: 0) {
+						Image(systemName: "person.fill.turn.down")
+						
+						VStack(spacing: 0) {
+							Text("b&b의 방")
+							Text("Aibiya님은 슈퍼호스트 입니다.")
+						}
+					}
+					HStack(alignment: .top, spacing: 0) {
+						Image(systemName: "person.fill.turn.down")
+						
+						VStack(spacing: 0) {
+							Text("b&b의 방")
+							Text("Aibiya님은 슈퍼호스트 입니다.")
+						}
+					}
+				}
+				
+				Spacer()
 			}
-			
+			.padding(.horizontal, 15)
 		}
-    }
+	}
 }
 
 struct ListingDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListingDetailView()
-    }
+	static var previews: some View {
+		ListingDetailView()
+	}
 }
