3fae33c2a7fd95d4a9ee9e58e86ef1124e358332
“Yoonds” <wkzks123@naver.com>
2024-01-05 00:10:36 +0900

    feat: 시트 클릭에 따른 바텀 시트 초기 구성 세팅

--- Yoondsbnb/Scene/Search/Explore/DestinationSearchView.swift
+++ Yoondsbnb/Scene/Search/Explore/DestinationSearchView.swift
@@ -7,11 +7,22 @@
 
 import SwiftUI
 
+// FIXME: 상태관리 뷰모델에서 할지 고민
+fileprivate enum DestinationSearchOptions {
+    
+    case basic
+    case location
+    case dates
+    case guests
+    
+}
+
 struct DestinationSearchView: View {
     
     @Binding var isShow: Bool
     
     @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .basic
     
     var body: some View {
         VStack(spacing: 16) {
@@ -21,9 +32,12 @@ struct DestinationSearchView: View {
                         isShow.toggle()
                     }
                 } label: {
-                    Image(systemName: "xmark.circle")
-                        .imageScale(.large)
-                        .foregroundColor(.black)
+                    Image(systemName: selectedOption == .basic
+                          ? "xmark.circle"
+                          : "arrowshape.backward.circle"
+                    )
+                    .imageScale(.large)
+                    .foregroundColor(.black)
                 }
                 .padding(.trailing, 110)
                 
@@ -36,41 +50,48 @@ struct DestinationSearchView: View {
             .padding(.horizontal, 16)
             
             VStack(alignment: .leading) {
-                Text("여행지를 알려주세요")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .imageScale(.small)
-                    TextField("여행지 검색", text: $destination)
-                        .font(.caption)
-                }
-                .frame(height: 44)
-                .padding(.horizontal)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 1.0)
-                        .foregroundColor(Color(.systemGray4))
-                }
-                .padding(.bottom, 15)
-                
-                // FIXME: 우측 패딩값을 덮어서 스크롤하도록 변경
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 15) {
-                        ForEach(1..<6) { continent in
-                            VStack(alignment: .leading) {
-                                Image("mainTest")
-                                    .resizable()
-                                    .frame(width: 130, height: 130)
-                                    .cornerRadius(8)
-                                Text("유럽")
-                                    .font(.caption)
+                if selectedOption == .basic {
+                    Text("여행지를 알려주세요")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("여행지 검색", text: $destination)
+                            .font(.caption)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundColor(Color(.systemGray4))
+                    }
+                    .onTapGesture {
+                        selectedOption = .location
+                    }
+                    .padding(.bottom, 15)
+                    
+                    // FIXME: 우측 패딩값을 덮어서 스크롤하도록 변경
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 15) {
+                            ForEach(1..<6) { continent in
+                                VStack(alignment: .leading) {
+                                    Image("mainTest")
+                                        .resizable()
+                                        .frame(width: 130, height: 130)
+                                        .cornerRadius(8)
+                                    Text("유럽")
+                                        .font(.caption)
+                                }
                             }
                         }
                     }
+                    .padding(.bottom, 10)
+                } else if selectedOption == .location {
+                    LocationBottomSheet()
                 }
-                .padding(.bottom, 10)
             }
             .padding()
             .background(.white)
@@ -78,8 +99,27 @@ struct DestinationSearchView: View {
             .shadow(radius: 10)
             .padding(.horizontal)
             
-            CollapsedPickerView(title: "날짜", description: "일주일")
-            CollapsedPickerView(title: "여행자", description: "게스트 추가")
+            VStack(spacing: 0) {
+                if selectedOption == .basic {
+                    CollapsedPickerView(title: "날짜", description: "일주일")
+                } else {
+                    DatesBottomSheet()
+                }
+            }
+            .onTapGesture {
+                selectedOption = .dates
+            }
+                
+            VStack(spacing: 0) {
+                if selectedOption == .basic {
+                    CollapsedPickerView(title: "여행자", description: "게스트 추가")
+                } else if selectedOption == .guests {
+                    GuestsBottomSheet()
+                }
+            }
+            .onTapGesture {
+                selectedOption = .guests
+            }
             
             Spacer()
             
@@ -110,7 +150,6 @@ struct DestinationSearchView: View {
             )
             .background(.white)
         }
-        .toolbar(.hidden, for: .tabBar)
         .background(.white)
         .opacity(0.97)
     }
@@ -137,6 +176,30 @@ private extension DestinationSearchView {
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .shadow(radius: 10)
             .padding(.horizontal)
+        }
+        
+    }
+    
+    struct LocationBottomSheet: View {
+        
+        var body: some View {
+            Text("여행 지바텀")
+        }
+        
+    }
+    
+    struct DatesBottomSheet: View {
+        
+        var body: some View {
+            Text("날짜 바텀")
+        }
+        
+    }
+    
+    struct GuestsBottomSheet: View {
+        
+        var body: some View {
+            Text("게스트 바텀")
         }
         
     }
