c3a35f9d0337c6e80eb089b76efcd95014ceb006
“Yoonds” <wkzks123@naver.com>
2024-01-18 01:02:29 +0900

    refactor: 코드 정리중 중간저장

--- Yoondsbnb/Scene/Search/Explore/DestinationSearchView.swift
+++ Yoondsbnb/Scene/Search/Explore/DestinationSearchView.swift
@@ -22,7 +22,7 @@ struct DestinationSearchView: View {
     @Binding var isShowDestinationSearchView: Bool
     
     @State private var isShowBottomSheet: Bool = false
-    @State private var selectedOption: DestinationSearchOptions = .basic
+    @State private var selectedOption: DestinationSearchOptions = .location
     @State private var destination = ""
     
     @FocusState private var isShowKeyboardField: Bool
@@ -64,101 +64,74 @@ struct DestinationSearchView: View {
             }
             
             VStack(spacing: 0) {
-                VStack(spacing: 0) {
-                    if selectedOption == .basic {
-                        HStack(spacing: 0) {
-                            Text("여행지를 알려주세요")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                            Spacer()
-                        }
-                        .padding(.bottom, 15)
-                    }
-                    
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(
-                            selectedOption == .basic
-                            ? Color.clear
-                            : Color(.systemGray6)
-                        )
-                        .stroke(selectedOption == .basic
-                                ? Color(.systemGray3)
-                                : Color.clear
-                        )
-                        .overlay {
-                            HStack(spacing: 15) {
-                                Image(systemName: "magnifyingglass")
-                                    .imageScale(.small)
-                                TextField("", text: $destination, prompt: Text("여행지 검색")
-                                    .font(.caption)
-                                    .foregroundColor(Color(.systemGray))
-                                )
-                                .keyboardType(.default)
-                                .focused($isShowKeyboardField)
-                            }
-                            .padding(15)
-                        }
-                        .frame(height: 50)
-                        .padding(.bottom, 20)
-                        .onTapGesture {
-                            withAnimation() {
-                                selectedOption = .location
-                            }
-                        }
-                }
-                .padding(
-                    EdgeInsets(
-                        top: 20,
-                        leading: 20,
-                        bottom: 0,
-                        trailing: 20
-                    )
-                )
-                
-                if selectedOption == .basic {
+                if selectedOption == .location {
                     VStack(spacing: 0) {
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 15) {
-                                ForEach(1..<6) { continent in
-                                    VStack(alignment: .leading) {
-                                        Image("mainTest")
-                                            .resizable()
-                                            .frame(width: 130, height: 130)
-                                            .cornerRadius(8)
-                                            .padding(.bottom, 10)
-                                        Text("유럽")
-                                            .font(.caption)
+                        VStack(spacing: 0) {
+                            HStack(spacing: 0) {
+                                Text("여행지를 알려주세요")
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                Spacer()
+                            }
+                            .padding(.bottom, 15)
+                        }
+                        .padding(
+                            EdgeInsets(
+                                top: 20,
+                                leading: 20,
+                                bottom: 0,
+                                trailing: 20
+                            )
+                        )
+                        
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .imageScale(.small)
+                            TextField("여행지 검색", text: $destination)
+                                .font(.caption)
+                        }
+                        .frame(height: 44)
+                        .padding(.horizontal)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(lineWidth: 1.0)
+                                .foregroundColor(Color(.systemGray4))
+                        }
+                        .padding(
+                            EdgeInsets(
+                                top: 0,
+                                leading: 20,
+                                bottom: 20,
+                                trailing: 20
+                            )
+                        )
+                        
+                        VStack(spacing: 0) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 15) {
+                                    ForEach(1..<6) { continent in
+                                        VStack(alignment: .leading) {
+                                            Image("mainTest")
+                                                .resizable()
+                                                .frame(width: 130, height: 130)
+                                                .cornerRadius(8)
+                                                .padding(.bottom, 10)
+                                            Text("유럽")
+                                                .font(.caption)
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                    .padding(
-                        EdgeInsets(
-                            top: 0,
-                            leading: 20,
-                            bottom: 20,
-                            trailing: 0
-                        )
-                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .shadow(radius: 10)
+                    .padding(.horizontal, 20)
+                } else {
+                    CollapsedPickerView(title: "여행지", description: "유연한 검색")
+                        .padding(.bottom, 20)
                 }
-                
-                if selectedOption == .location {
-                    Spacer()
-                }
-                // FIXME: 여기까지 확인 완료 한줄씩 체크하면서 코드 확인
             }
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .shadow(radius: 10)
-            .padding(
-                EdgeInsets(
-                    top: 0,
-                    leading: selectedOption == .basic ? 20 : 0,
-                    bottom: selectedOption == .basic ? 20 : 0,
-                    trailing: selectedOption == .basic ? 20 : 0
-                )
-            )
             
             if selectedOption == .location {
                 Spacer()
@@ -166,59 +139,58 @@ struct DestinationSearchView: View {
             
             if selectedOption == .basic {
                 VStack(spacing: 0) {
-                    
-                    VStack(spacing: 0) {
-                        CollapsedPickerView(title: "날짜", description: "일주일")
+                    CollapsedPickerView(title: "날짜", description: "일주일")
+                        .padding(.horizontal, 20)
+                }
+                .padding(.bottom, 20)
+                .onTapGesture {
+                    withAnimation {
+                        selectedOption = .dates
                     }
-                    .padding(.bottom, 20)
-                    .onTapGesture {
-                        withAnimation {
-                            selectedOption = .dates
-                        }
-                    }
-                    
-                    VStack(spacing: 0) {
-                        CollapsedPickerView(title: "여행자", description: "게스트 추가")
-                    }
-                    .onTapGesture {
-                        withAnimation {
-                            selectedOption = .guests
-                        }
-                    }
-                    
-                    Spacer()
-                    Divider()
-                    HStack(spacing: 0) {
-                        Text("전체 삭제")
-                            .underline()
-                        Spacer()
-                        Button {
-                            print("검색") // TODO: 검색기능 추가
-                        } label: {
-                            HStack(spacing: 7) {
-                                Image(systemName: "magnifyingglass")
-                                    .imageScale(.medium)
-                                Text("검색")
-                            }
-                            .padding(
-                                EdgeInsets
-                                    .init(top: 10, leading: 12, bottom: 10, trailing: 15)
-                            )
-                            .background(.red)
-                            .foregroundColor(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                        }
-                    }
-                    .padding(
-                        EdgeInsets(
-                            top: 20,
-                            leading: 20,
-                            bottom: 30,
-                            trailing: 20
-                        )
-                    )
                 }
             }
+            
+            VStack(spacing: 0) {
+                CollapsedPickerView(title: "여행자", description: "게스트 추가")
+                    .padding(.horizontal, 20)
+            }
+            .onTapGesture {
+                withAnimation {
+                    selectedOption = .guests
+                }
+            }
+            
+            Spacer()
+            Divider()
+            HStack(spacing: 0) {
+                Text("전체 삭제")
+                    .underline()
+                Spacer()
+                Button {
+                    print("검색") // TODO: 검색기능 추가
+                } label: {
+                    HStack(spacing: 7) {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.medium)
+                        Text("검색")
+                    }
+                    .padding(
+                        EdgeInsets
+                            .init(top: 10, leading: 12, bottom: 10, trailing: 15)
+                    )
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            .padding(
+                EdgeInsets(
+                    top: 20,
+                    leading: 20,
+                    bottom: 30,
+                    trailing: 20
+                )
+            )
         }
         .toolbar(.hidden, for: .tabBar)
         .ignoresSafeArea(edges: .bottom)
@@ -237,17 +209,15 @@ private extension DestinationSearchView {
         var body: some View {
             HStack(spacing: 0) {
                 Text(title)
-                    .font(.caption)
                     .foregroundColor(Color.gray)
                 Spacer()
                 Text(description)
-                    .font(.caption)
             }
-            .padding(17)
+            .font(.subheadline)
+            .padding(25)
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .shadow(radius: 10)
-            .padding(.horizontal)
         }
         
     }
