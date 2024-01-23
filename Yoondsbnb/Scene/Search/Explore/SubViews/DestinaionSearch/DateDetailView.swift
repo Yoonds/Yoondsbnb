8d307b82d2e2fd1cae0509e9c4cb147ad27a2893
“Yoonds” <wkzks123@naver.com>
2024-01-24 00:34:28 +0900

    feat: 달력화면 기본 구현 중

---
+++ Yoondsbnb/Scene/Search/Explore/SubViews/DestinaionSearch/DateDetailView.swift
@@ -0,0 +1,144 @@
+//
+//  DateDetailView.swift
+//  Yoondsbnb
+//
+//  Created by Yoon Daesung on 1/23/24.
+//
+
+import SwiftUI
+
+struct DateDetailView: View {
+    
+    enum PickerKind: CaseIterable, CustomDebugStringConvertible, Identifiable {
+        case date, month, flexible
+        
+        var id: Self { self }
+        var debugDescription: String {
+            switch self {
+            case .date:
+                return "날짜 지정"
+            case .month:
+                return "월 단위"
+            case .flexible:
+                return "유연한 일정"
+            }
+        }
+    }
+    
+    @State private var selectedDate = Date()
+    @State private var selctedPicker: PickerKind = .date
+    
+    @Binding var selectedOption: DestinationSearchOptions
+    
+    var body: some View {
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Text("언제 여행 가실 예정인가요?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                .padding(.bottom, 15)
+                
+                Picker("picker date", selection: $selctedPicker) {
+                    ForEach(PickerKind.allCases) { picker in
+                        Text(picker.debugDescription)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .frame(width: proxy.size.width / 1, height: proxy.size.height / 12)
+                
+                switch selctedPicker {
+                case .date:
+                    VStack(spacing: 0) {
+                        DatePicker(
+                                "Start Date",
+                                selection: $selectedDate,
+                                in: Date()...,
+                                displayedComponents: [.date]
+                            )
+                            .datePickerStyle(.graphical)
+                        Divider()
+                            .padding(.bottom, proxy.size.height / 35)
+                        
+                        // TODO: 슬라이드 버튼으로 수정 예정
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 12) {
+                                BottomAddDateButton(image: nil, title: "정확한 날짜")
+                                BottomAddDateButton(image: "plusminus", title: "1")
+                                BottomAddDateButton(image: "plusminus", title: "2")
+                                BottomAddDateButton(image: "plusminus", title: "3")
+                                BottomAddDateButton(image: "plusminus", title: "3")
+                                BottomAddDateButton(image: "plusminus", title: "7")
+                            }
+                        }
+//                        .padding(.bottom, proxy.size.height / 35)
+                    }
+                case .month:
+                    Text("월 뷰")
+                case .flexible:
+                    Text("유연 뷰")
+                }
+
+                Spacer()
+                
+                Divider()
+                    .padding(.bottom, proxy.size.height / 35)
+                
+                HStack(spacing: 0) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("건너 뛰기")
+                            .underline(true)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        selectedOption = .guests
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 13)
+                            .fill(Color.black)
+                            .frame(width: proxy.size.width / 2.5, height: proxy.size.height / 14)
+                            .overlay {
+                                Text("다음")
+                                    .foregroundStyle(.white)
+                            }
+                            
+                    })
+                }
+            }
+        }
+    }
+}
+
+extension DateDetailView {
+    
+    struct BottomAddDateButton: View {
+        
+        let image: String?
+        let title: String
+        
+        var body: some View {
+            HStack(spacing: 10) {
+                if let image = image {
+                    Image(systemName: image)
+                    Text(title + "일")
+                } else {
+                    Text(title)
+                }
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+            .overlay {
+                Capsule()
+                    .stroke(lineWidth: 1.0)
+                    .foregroundColor(Color(.systemGray4))
+            }
+        }
+        
+    }
+    
+}
--- Yoondsbnb/Scene/Search/Explore/SubViews/DestinaionSearch/DestinationSearchView.swift
+++ Yoondsbnb/Scene/Search/Explore/SubViews/DestinaionSearch/DestinationSearchView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 // FIXME: 상태관리 뷰모델에서 할지 고민
-fileprivate enum DestinationSearchOptions {
+enum DestinationSearchOptions {
     
     case location
     case dates
@@ -16,170 +16,163 @@ fileprivate enum DestinationSearchOptions {
     
 }
 
+// TODO: 전체 UI Geometry사용하여 개선 예정
 struct DestinationSearchView: View {
 
-    @State private var destination = ""
     @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var destination = ""
     
     @Binding var isShowDestinationSearchView: Bool
     
     var body: some View {
-        VStack(spacing: 0) {
-            // 상단 탭
-            HStack(spacing: 0) {
-                Text("x")
-                    .padding(8)
-                    .foregroundColor(.black)
-                    .background(Color.white)
-                    .clipShape(Circle())
-                    .overlay {
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 1)
-                    }
-                Spacer()
-                Text("숙소")
-                    .padding(.trailing, 20)
-                Text("체험")
-                Spacer()
-            }
-            .padding(.horizontal, 5)
-            
-            // location
-            if selectedOption == .location {
-                VStack(spacing: 0) {
-                        VStack(spacing: 0) {
-                            HStack(spacing: 0) {
-                                Text("여행지를 알려주세요")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                Spacer()
-                            }
-                            .padding(.bottom, 15)
-                            
-                            HStack {
-                                Image(systemName: "magnifyingglass")
-                                    .imageScale(.small)
-                                TextField("여행지 검색", text: $destination)
-                                    .font(.caption)
-                            }
-                            .frame(height: 40)
-                            .padding(5)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 8)
-                                    .stroke(lineWidth: 1.0)
-                                    .foregroundColor(Color(.systemGray4))
-                            }
-                            .padding(.bottom, 25)
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                // 상단 탭
+                HStack(spacing: 0) {
+                    Text("x")
+                        .padding(8)
+                        .foregroundColor(.black)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 1)
                         }
-                        .padding(.horizontal, 20)
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 15) {
-                                ForEach(1..<6) { continent in
-                                    VStack(alignment: .leading) {
-                                        Image("mainTest")
-                                            .resizable()
-                                            .frame(width: 130, height: 130)
-                                            .cornerRadius(8)
-                                            .padding(.bottom, 10)
-                                        Text("유럽")
-                                            .font(.caption)
+                    Spacer()
+                    Text("숙소")
+                        .padding(.trailing, 20)
+                    Text("체험")
+                    Spacer()
+                }
+                .padding(.horizontal, 5)
+                
+                // location
+                if selectedOption == .location {
+                    VStack(spacing: 0) {
+                            VStack(spacing: 0) {
+                                HStack(spacing: 0) {
+                                    Text("여행지를 알려주세요")
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
+                                    Spacer()
+                                }
+                                .padding(.bottom, 15)
+                                
+                                HStack {
+                                    Image(systemName: "magnifyingglass")
+                                        .imageScale(.small)
+                                    TextField("여행지 검색", text: $destination)
+                                        .font(.caption)
+                                }
+                                .frame(height: proxy.size.height / 20)
+                                .padding(5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(lineWidth: 1.0)
+                                        .foregroundColor(Color(.systemGray4))
+                                }
+                                .padding(.bottom, 25)
+                            }
+                            .padding(.horizontal, 20)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 15) {
+                                    ForEach(1..<6) { continent in
+                                        VStack(alignment: .leading) {
+                                            Image("mainTest")
+                                                .resizable()
+                                                .frame(
+                                                    width: proxy.size.width / 3.5,
+                                                    height: proxy.size.width / 3.5
+                                                )
+                                                .cornerRadius(8)
+                                                .padding(.bottom, 10)
+                                            Text("유럽")
+                                                .font(.caption)
+                                        }
                                     }
                                 }
                             }
-                        }
-                        .padding(.leading, 20)
-                }
-                .padding(EdgeInsets(top: 25, leading: 0, bottom: 15, trailing: 0))
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .shadow(radius: 5)
-                .padding(
-                    EdgeInsets(
-                        top: 20,
-                        leading: selectedOption == .location ? 15 : 20,
-                        bottom: 20,
-                        trailing: selectedOption == .location ? 15 : 20
-                    )
-                )
-            } else {
-                CollapsedPickerView(title: "여행지", description: "유연한 검색")
+                            .padding(.leading, proxy.size.width / 20)
+                    }
+                    .padding(EdgeInsets(top: 25, leading: 0, bottom: 15, trailing: 0))
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .shadow(radius: 5)
                     .padding(
                         EdgeInsets(
-                            top: 20, leading: 20, bottom: 20, trailing: 20
+                            top: 20,
+                            leading: selectedOption == .location ? 15 : 20,
+                            bottom: 20,
+                            trailing: selectedOption == .location ? 15 : 20
                         )
                     )
-                    .onTapGesture {
-                        withAnimation(.snappy) {
-                            selectedOption = .location
+                } else {
+                    CollapsedPickerView(title: "여행지", description: "유연한 검색")
+                        .padding(
+                            EdgeInsets(
+                                top: 20, leading: 20, bottom: 20, trailing: 20
+                            )
+                        )
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                selectedOption = .location
+                            }
                         }
+                }
+                
+                if selectedOption == .dates {
+                    VStack(spacing: 0) {
+                        DateDetailView(selectedOption: $selectedOption)
+                            .padding(.horizontal, 20)
+                            .frame(height: proxy.size.height / 1.25)
                     }
-            }
-            
-            if selectedOption == .dates {
-                VStack(spacing: 0) {
-                        VStack(spacing: 0) {
-                            HStack(spacing: 0) {
-                                Text("언제 여행 가실 예정인가요?")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                Spacer()
-                            }
-                            .padding(.bottom, 15)
-                            
-                            HStack {
-                                Text("날짜 지정")
-                                Text("월 단위")
-                                Text("유연한 일정")
+                    .padding(EdgeInsets(top: 25, leading: 0, bottom: 15, trailing: 0))
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .shadow(radius: 5)
+                    .padding(
+                        EdgeInsets(
+                            top: 0,
+                            leading: selectedOption == .location ? 15 : 20,
+                            bottom: 20,
+                            trailing: selectedOption == .location ? 15 : 20
+                        )
+                    )
+                } else {
+                    CollapsedPickerView(title: "날짜", description: "일주일")
+                        .padding(
+                            EdgeInsets(
+                                top: 0, leading: 20, bottom: 20, trailing: 20
+                            )
+                        )
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                selectedOption = .dates
                             }
                         }
+                }
+                
+                if selectedOption == .guests {
+                    Text("여행자 뷰")
+                } else if selectedOption == .location {
+                    CollapsedPickerView(title: "여행자", description: "게스트 추가")
                         .padding(.horizontal, 20)
-                        Text("캘릭더 뷰")
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                selectedOption = .guests
+                            }
+                        }
+                        
                 }
-                .padding(EdgeInsets(top: 25, leading: 0, bottom: 15, trailing: 0))
-                .background(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .shadow(radius: 5)
-                .padding(
-                    EdgeInsets(
-                        top: 0,
-                        leading: selectedOption == .location ? 15 : 20,
-                        bottom: 20,
-                        trailing: selectedOption == .location ? 15 : 20
-                    )
-                )
-            } else {
-                CollapsedPickerView(title: "날짜", description: "일주일")
-                    .padding(
-                        EdgeInsets(
-                            top: 0, leading: 20, bottom: 20, trailing: 20
-                        )
-                    )
-                    .onTapGesture {
-                        withAnimation(.snappy) {
-                            selectedOption = .dates
-                        }
-                    }
+                Spacer()
             }
-            
-            if selectedOption == .guests {
-                Text("여행자 뷰")
-            } else if selectedOption == .location {
-                CollapsedPickerView(title: "여행자", description: "게스트 추가")
-                    .padding(.horizontal, 20)
-                    .onTapGesture {
-                        withAnimation(.snappy) {
-                            selectedOption = .guests
-                        }
-                    }
-                    
-            }
-            Spacer()
+            .toolbar(.hidden, for: .tabBar)
+            .ignoresSafeArea(edges: .bottom)
+            .navigationBarHidden(true)
+            .background(.white)
+
         }
-        .toolbar(.hidden, for: .tabBar)
-        .ignoresSafeArea(edges: .bottom)
-        .navigationBarHidden(true)
-        .background(.white)
     }
     
 }
